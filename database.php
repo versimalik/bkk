d78b0400c23ee5edc0c8f0c18b2bfa55d7e87b25
abulubabah <rezamalikdrive@gmail.com>
2021-08-23 23:55:51 +0700

    db updated

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','root021');
+define('DB_USER','u6519370_ypippijkt');
+define('DB_PASS','ypippijkt');
 define('DB_NAME','u6519370_daful');
 
 $db_conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
